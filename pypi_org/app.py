1a8f0db9fd14b02e3a845bd3f414eaa3698cc259
Ivanir Efi <efi.ivanir@gmail.com>
2021-03-10 11:44:32 +0200

    Middle of ch 6

---
+++ pypi_org/app.py
@@ -0,0 +1,24 @@
+import os
+import sys
+import flask
+folder = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.insert(0, folder)
+
+app = flask.Flask(__name__)
+
+
+def register_blueprints():
+    from pypi_org.views import home_views
+    from pypi_org.views import package_views
+
+    app.register_blueprint(home_views.blueprint)
+    app.register_blueprint(package_views.blueprint)
+
+
+def main():
+    register_blueprints()
+    app.run(debug=True)
+
+
+if __name__ == '__main__':
+    main()
