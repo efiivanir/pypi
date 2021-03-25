5ec311ea461e117e8500e6e4ea1e5495102fd0b1
Ivanir Efi <efi.ivanir@gmail.com>
2021-03-25 13:29:31 +0200

    End ch08

--- pypi_org/app.py
+++ pypi_org/app.py
@@ -1,7 +1,8 @@
 import os
 import sys
 import flask
-folder = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+
+folder = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 sys.path.insert(0, folder)
 
 app = flask.Flask(__name__)
@@ -12,13 +13,13 @@ def register_blueprints():
     from pypi_org.views import package_views
     from pypi_org.views import cms_views
 
- #   from pypi_org.views import account_views
+    #   from pypi_org.views import account_views
 
     app.register_blueprint(home_views.blueprint)
     app.register_blueprint(package_views.blueprint)
     app.register_blueprint(cms_views.blueprint)
 
-    #app.register_blueprint(account_views.blueprint)
+    # app.register_blueprint(account_views.blueprint)
 
 
 def main():
@@ -26,5 +27,5 @@ def main():
     app.run(debug=True)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
